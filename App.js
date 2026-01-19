32f5d9dbae892026d45f98ff3cac15da6bea930d
hennasalmi02 <hennasalmi02@gmail.com>
2026-01-19 19:26:49 +0200

    Laskin lisätty

--- App.js
+++ App.js
@@ -2,10 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+
+  const sum = () => {
+    return (
+      "Result: " + (parseFloat(number1) + parseFloat(number2))
+    );
+  }
+
+  const difference = () => {
+    return (
+      "Result: " + (parseFloat(number1) - parseFloat(number2))
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <Text style={{ fontSize: 18, fontWeight: 'bold'}}>This is text</Text>   
+      </View>
+      <View style={{flex: 2}}>
+        <TextInput
+        placeholder='Enter a number'
+        onChangeText={number1 => setNumber1(number1)} 
+        value={number1}/>
+        <TextInput
+        placeholder='Enter a number'
+        onChangeText={number2 => setNumber2(number2)} 
+        value={number2}/>  
+      </View>
+      <View style={{flex: 1}}>
+        <Button onPress={sum} title="+" />
+        <Button onPress={difference} title="-" />
+      </View>
     </View>
   );
 }
