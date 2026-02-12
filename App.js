2e4e53b718b48ef19b9219efb78ad73048324294
hennasalmi02 <hennasalmi02@gmail.com>
2026-02-12 20:48:11 +0200

    Reseptien hakutehtävä suoritettu

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './HomeScreen';
-import HistoryScreen from './HistoryScreen.js';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList, Image } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+function App() {
 
-export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [receipts, setReceipts] = useState([]);
+
+  const handleFetch = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+    .then(response => {
+      if (!response.ok)
+        throw new Error("Error in fetch:" + response.statusText);
+      
+      return response.json()
+    })
+    .then(data => setReceipts(data.meals))
+    .catch(err => console.error(err));    
+  }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="History" component={HistoryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <View style={styles.container}>
+        <TextInput
+          style={{ fontSize: 18, width: 200 }}
+          placeholder='keyword'
+          value={keyword}
+          onChangeText={text => setKeyword(text)} />
+      </View>
+      <View style={styles.container}>
+        <Button title="Find" onPress={handleFetch} />
+        <FlatList
+          data={receipts}
+          keyExtractor={(item) => item.idMeal}
+          renderItem={({ item }) => <View>
+            <Text style={{ fontSize: 18, fontWeight: "bold"  }}>
+              {item.strMeal}
+            </Text>
+            <Image
+              style={{ width: 100, height: 100 }}
+              source={{ uri: item.strMealThumb }}
+            />
+          </View>} />
+      </View></>
   );
 }
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    color: '#333',
+  },
+};
+
+export default App;
