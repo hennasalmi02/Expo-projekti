fe4f49cc5fab12d9ef651889d3e2e0c3bcb4d481
hennasalmi02 <hennasalmi02@gmail.com>
2026-03-21 16:34:06 +0200

    Ostoslistaa päivitetty (SQLite)

--- App.js
+++ App.js
@@ -1,47 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import * as SQLite from 'expo-sqlite';
 
 export default function App() {
 
-  const [item, setItem] = useState("");
+  const db = SQLite.openDatabaseSync('productdb');
+
+  const [product, setProduct] = useState("");
+  const [amount, setAmount] = useState("");
   const [items, setItems] = useState([]);
 
-  const handlePress = () => {
-    setItems([...items, { key: item }]);
-    setItem("");
+  const initialize = async () => {
+    try {
+      await db.execAsync(`
+        CREATE TABLE IF NOT EXISTS product (id INTEGER PRIMARY KEY NOT NULL, product TEXT, amount TEXT);
+      `);
+    } catch (error) {
+      console.error('Could not open database', error);
+    }
   }
 
-  const handleClear = () => {
-    setItems([]);
+  useEffect(() => 
+    { initialize();
+      updateList();
+     }, []);
+
+  const saveItem = async () => {
+    try {
+      await db.runAsync('INSERT INTO product (product, amount) VALUES (?, ?)', product, amount);
+      await updateList();
+    } catch (error) {
+      console.error('Could not add item', error);
+    }
+  };
+
+  const updateList = async () => {
+    try {
+      const list = await db.getAllAsync('SELECT * from product');
+      setItems(list);
+    } catch (error) {
+      console.error('Could not get items', error);
+    }
+  }
+
+  const deleteItem = async (id) => {
+    try {
+      await db.runAsync('DELETE FROM product WHERE id=?', id);
+      await updateList();
+    }
+    catch (error) {
+      console.error('Could not delete item', error);
+    }
   }
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={{ fontSize: 18, fontWeight: 'bold'}}>Shopping list</Text>   
-        <TextInput
-          placeholder='Enter an item'
-          onChangeText={text => setItem(text)} 
-          value={item}/>
-      </View>
-      <View>
-        <Button onPress={handlePress} title="Add" />
-        <Button onPress={handleClear} title="Clear" />
-        <FlatList 
-          data={items} 
-          renderItem={({item}) => <Text>{item.key}</Text>} 
-        />
-      </View>
+      <Text style={{ fontSize: 18, fontWeight: 'bold'}}>Shopping list</Text>   
+      <TextInput 
+        placeholder='Product' 
+        onChangeText={product => setProduct(product)}
+        value={product}/> 
+      <TextInput 
+        placeholder='Amount' 
+        keyboardType='numeric' 
+        onChangeText={amount => setAmount(amount)}
+        value={amount}/> 
+      <Button onPress={saveItem} title="Save" />
+      <FlatList
+        style={{ width: '100%' }}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) =>
+        <View>
+          <Text>{item.product}</Text>
+          <Text>{item.amount} </Text>
+          <Text style={{ color: '#ff0000' }} onPress={() => deleteItem(item.id)}>bought</Text>
+        </View>
+      }
+      data={items}
+      />
     </View>
+        
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    padding: 50,
   },
 });
